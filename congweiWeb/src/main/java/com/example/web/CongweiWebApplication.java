18f8a620df23916a2c5780c13d4f205fb94d9059
congwweiGithub <congwei@k-stamp.co.jp>
2022-05-16 10:28:28 +0900

    create a new springBoot project

---
+++ congweiWeb/src/main/java/com/example/web/CongweiWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongweiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongweiWebApplication.class, args);
+	}
+
+}
